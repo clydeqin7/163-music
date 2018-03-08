5f6e4092e9da8d1b27d805826c5689af4a549aba
clyde <clydeqin7@foxmail.com>
2018-03-08 21:50:02 +0800

    song播放暂停

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -7,8 +7,17 @@
         render(data){
             let {song, status} = data
             this.$el.find('audio').attr('src', song.url)
+            console.log(song.url)
+            console.log(this.$el.find('audio'))
+        },
+        play(){
+            this.$el.find('audio') [0].play()
+            this.$el.find('.disc-container').addClass('playing')
+        },
+        pause(){
+            this.$el.find('audio')[0].pause()
+           this.$el.find('.disc-container').removeClass('playing')            
         }
-
     }
     let model = {
         data:{
@@ -38,7 +47,14 @@
                 this.view.render(this.model.data)
             })
         },
-        bindEvents(){},
+        bindEvents(){
+            this.view.$el.on('click', '.icon-play', ()=>{
+                this.view.play()
+            })
+            this.view.$el.on('click', '.icon-pause', ()=>{
+                this.view.pause()
+            })
+        },
         getSongId(){
             let search = window.location.search
             if(search.indexOf('?') === 0){
